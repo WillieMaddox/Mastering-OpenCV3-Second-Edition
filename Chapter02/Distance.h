b927a81542c4cb328b0cbbcbaa8d356657b123da
WillieMaddox <willie.maddox@gmail.com>
2017-06-07 18:48:58 -0500

    Vec3b bug fix.

--- Chapter02/Distance.h
+++ Chapter02/Distance.h
@@ -43,14 +43,14 @@ private:
     
     std::vector<CloudPoint> pointcloud;
     std::vector<cv::KeyPoint> correspImg1Pt;
-    
+    std::vector<cv::Vec3b> pointcloudrgb;
     bool features_matched;
 public:
     std::vector<cv::Point3d> getPointCloud() { return CloudPointsToPoints(pointcloud); }
     const cv::Mat& getleft_im_orig() { return left_im_orig; }
     const cv::Mat& getright_im_orig() { return right_im_orig; }
     const std::vector<cv::KeyPoint>& getcorrespImg1Pt() { return correspImg1Pt; }
-    const std::vector<cv::Vec3b>& getPointCloudRGB() { return std::vector<cv::Vec3b>();}
+    const std::vector<cv::Vec3b>& getPointCloudRGB() { return pointcloudrgb; }
         //c'tor
     Distance(const cv::Mat& left_im_, const cv::Mat& right_im_):
         features_matched(false)
